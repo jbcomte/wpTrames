4bb15b9eddcecf21e83e13d41a54822790f7b5ce
jbcomte <jb2c.consulting@gmail.com>
2016-12-01 14:48:20 +0100

    Add CORS header

--- testRest.php
+++ testRest.php
@@ -2,6 +2,7 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date dans le passé
 header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
 
 $return = array('data' => 'Hello '.$_POST['firstname'], 'code' => 200);
 
